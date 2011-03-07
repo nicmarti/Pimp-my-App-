0ca65f877fa1f4d9b4c41884fcfcbb4420e86c4e
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2011-03-07 22:19:54 +0100

    Authentification et autorisation

---
+++ app/controllers/SecureController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+/**
+ * TODO
+ *
+ * @author Nicolas Martignole
+ * @since 07/03/11 21:28
+ */
+public class SecureController {
+}
---
+++ app/models/User.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * TODO
+ *
+ * @author Nicolas Martignole
+ * @since 07/03/11 21:29
+ */
+public class User {
+}
